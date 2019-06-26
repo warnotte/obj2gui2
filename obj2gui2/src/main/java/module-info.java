080ec048743498622cf0a9f82292d8a43a884bd4
warnotte <renaud@warnotte.be>
2019-06-26 18:49:07 +0200

    ---

--- obj2gui2/src/main/java/module-info.java
+++ obj2gui2/src/main/java/module-info.java
@@ -21,14 +21,14 @@ module obj2gui2
 	exports org.warnotte.obj2gui2.Tests.SimpleType;
 	exports org.warnotte.obj2gui2.Tests.SimpleTypeDecimalFormattedFields;
 
-	requires WaxLibrary2;
+	requires transitive WaxLibrary2;
 	requires java.desktop;
 	requires java.xml;
 	requires transitive jdom2;
-	requires miglayout;
-	requires swingx.all;
-	requires vecmath;
-	requires waxlibswingcomponentsMVN;
+	requires transitive miglayout;
+	requires transitive swingx.all;
+	requires transitive vecmath;
+	requires transitive waxlibswingcomponentsMVN;
 	requires transitive org.apache.logging.log4j;
 	requires transitive org.apache.logging.log4j.core;
 }
